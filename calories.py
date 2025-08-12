f8cacf45d29e00f7fce3f316db406c3623b8a5c3
velveetax <luchak1313@gmail.com>
2025-08-12 18:40:28 +0200

    Add files via upload

--- calories.py
+++ calories.py
@@ -7,24 +7,41 @@ products = {
     "potato": 87,
     "poridge": 73
 }
+print(f"Here is our product list: {products}")
 calories_sum = 0
 max_c = float(input("What is your goal in calories today? "))
 while True:
-    name = input("Enter your product or exit: ")
+    name = input("Enter your product or 'exit' to add a new one: ").lower()
     if name == "exit":
-        break
-    if name not in products:
-        print("Sorry, we don't have such product in our base yet :(")
-        continue
-    weight = float(input("How many grammes? "))
-    calories_100g = products[name]
+        new_product = input("Do you want to add your product? Answer yes or no. ").lower()
+        if new_product == "no":
+            print("Okay!")
+            break
+        elif new_product == "yes":
+            product_enter = input("Enter your product. ").lower()
+            calories_100g = float(input("How much calories does it have in 100g? "))
+            products[product_enter] = calories_100g
+            print(f"New list: {products}")
+    else:
+        calories_100g = products[name]
+    weight = float(input("How many grammes did you eat? "))
     calories = calories_100g * weight / 100
     calories_sum += calories
     calories_left = max_c - calories_sum
+    print(f"You added {calories:.1f} calories. In all: {calories_sum:.1f}/{max_c}")
     if calories_left > 0:
         print(f"You still have {calories_left:.1f} calories left! Keep going!")
     elif calories_left == 0:
         print("You got it!")
     else:
         print(f"Oops, surplus! You ate {-calories_left:.1f} calories more.")
-    print(f"You added {calories:.1f} calories. In all: {calories_sum:.1f}/{max_c}")
+        break
+    info = input("Do you want to exit? Answer yes or no. ").lower()
+    if info == "no":
+        print("Okay! Let's continue!")
+    elif info == "yes":
+        print("Bye!")
+        break
+    else:
+        print("I don't understand.")
+        break
